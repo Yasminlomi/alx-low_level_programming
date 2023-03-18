39c69b79dc9ff02a50a1cb861f1f492101e554de
Yasminlomi <yasmin.mossad@gmail.com>
2023-03-17 17:43:40 -0700

    c loop

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entery point
+*Description: 'Smile in the mirror'
+*Return: always 0
+*/
+int main(void)
+{
+int n = 122;
+
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
