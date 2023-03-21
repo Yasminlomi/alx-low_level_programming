7787a521ad663d3e96fe4943e6bce7dc0c1e97d1
Yasminlomi <yasmin.mossad@gmail.com>
2023-03-21 12:52:55 -0700

    nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
- *main - Entry point
- *print_alphabet - print all alphabet in lowercase
- *Return: always 0
- */
+ * print_alphabet - print all alphabet in lowercase
+*/
 void print_alphabet(void)
 {
 char letter;
